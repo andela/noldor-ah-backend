dfacd0a20a225e9c06fe6ecdb9c64e5e240bd205
Chukwuemeka Ajima <chukwuemekaajima@Chukwuemekas-MacBook-Pro.local>
2018-10-26 11:32:09 +0100

    chore(initialSetup): configure PostgreSQL database - setup sequelize, sequelize-cli and sequelizerc files - generate models, migration files and config File - include a .env file to store database credentials - add scripts to the package.json file to migrate database tables and drop database HTMLTableSectionElement - added a babelHook.js file to handle ES6 synthax for sequelize - updated the babelrc to include module export plugins - install babel-register, babel-plugin-add-module-exports sequelize-cli modules, pg, pg-hstore, sequelize modules - git ignore .env file
    [(Finishes) #161290889]

---
+++ server/src/net/config/config.js
@@ -0,0 +1,29 @@
+import dotenv from 'dotenv';
+
+dotenv.config();
+
+const config = {
+  development: {
+    username: process.env.DB_USER,
+    password: process.env.DB_PASSWORD,
+    database: process.env.DB_NAME,
+    host: process.env.HOST,
+    port: process.env.PORT,
+    dialect: 'postgres',
+    logging: false,
+    operatorsAliases: false
+  },
+  test: {
+    username: process.env.DB_USER,
+    password: process.env.DB_PASSWORD,
+    database: process.env.DB_NAME_TEST,
+    host: process.env.HOST,
+    dialect: 'postgres'
+  },
+  production: {
+    use_env_variable: 'DATABASE_URL',
+    dialect: 'postgres'
+  }
+};
+
+export default config;
