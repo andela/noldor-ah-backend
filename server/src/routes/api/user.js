d9a7b409edaacf687ff39ee90339b3d3c2209fc3
Chukwuemeka Ajima <chukwuemekaajima@Chukwuemekas-MacBook-Pro.local>
2018-10-30 13:40:24 +0100

    feat(Authentication):user receive token on registration
         - issue user a valid token on successful registration
         - middleware to validate user token
         - test to check for token availability
         - user login functionality
         - user signup functionality
         - test to register and login a user
         - upgrade babel configuration
         - adjust test script to accommodate db migration
    
         [Delivers #161290897]

--- server/src/routes/api/user.js
+++ server/src/routes/api/user.js
@@ -1,9 +1,10 @@
 import express from 'express';
 import UserController from '../../controllers/UserController';
+import Validation from '../../middlewares/validation';
 
 const router = express.Router();
 
-router.post('/users/register', UserController.register);
-router.post('/users/login/', UserController.login);
+router.post('/users/register', Validation.signupValidation, UserController.register);
+router.post('/users/login/', Validation.loginValidation, UserController.login);
 
 export default router;
