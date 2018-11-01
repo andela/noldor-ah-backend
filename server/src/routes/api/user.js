29ba9d894cb589236a6c59fa92c022186be7175f
Hope Uwa <hopeuwa@Hopes-MacBook-Pro.local>
2018-11-01 11:00:19 +0100

    feat(articles): create,  read, update and delete articles         - Write test for endpoints         - Implemented relationship in model and migration         - Validationhelpers for articles inputs         - Create article functionalities         - Update article functionalities         - Read article functionalities         - Delete article functionality
            [Finishes #161290903]

--- server/src/routes/api/user.js
+++ server/src/routes/api/user.js
@@ -1,7 +1,6 @@
 import express from 'express';
 import UserController from '../../controllers/UserController';
 import Validation from '../../middlewares/validation';
-<<<<<<< HEAD
 import userToken from '../../middlewares/token';
 
 const router = express.Router();
@@ -31,20 +30,5 @@ router.post('/users/login/', loginValidation, login);
 router.get('/users/', validateToken, getAllUser);
 router.put('/users/forgot', forgotPasswordValidation, forgetPassword);
 router.post('/users/forgot/:hash', resetPasswordValidation, resetPassword);
-=======
-import Token from '../../middlewares/token';
-
-const router = express.Router();
-
-router.post('/users/register', Validation.signupValidation, UserController.register);
-<<<<<<< HEAD
-router.post('/users/login', Validation.loginValidation, UserController.login);
-=======
-router.post('/users/login/', Validation.loginValidation, UserController.login);
-router.get('/users/:userId/profiles', UserController.viewUserProfile);
-router.put('/users/:userId/profiles', Token.validateToken, UserController.editUserProfile);
-router.delete('/users/:userId/profiles', Token.validateToken, UserController.deleteUser);
->>>>>>> feature(view/edit profile): enable user view/edit profile
->>>>>>> feature(view/edit profile): enable user view/edit profile
 
 export default router;
