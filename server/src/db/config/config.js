3c7714590ffc2d48adeb51d17af3eaef78da040a
Ovie Okeh <kevinokeh@gmail.com>
2018-11-04 00:09:18 +0100

    feat(search): add search functionality - add index and triggers to articles table - implement search by keywords alone - implement search by both keywords and filters - add tests for search feature - add search inputs validation [Finishes #161290903]

--- server/src/db/config/config.js
+++ server/src/db/config/config.js
@@ -14,13 +14,9 @@ const config = {
     operatorsAliases: false
   },
   test: {
-    username: process.env.DB_USER,
-    password: process.env.DB_PASSWORD,
-    database: process.env.DB_NAME_TEST,
-    host: process.env.HOST,
+    use_env_variable: 'DATABASE_URL',
     port: '5432',
-    dialect: 'postgres',
-    logging: false,
+    dialect: 'postgres'
   },
   production: {
     use_env_variable: 'DATABASE_URL',
