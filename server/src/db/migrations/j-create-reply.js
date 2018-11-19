cb9defbce9d8bf663539b6a331d5efde7449ade1
Ovie Okeh <kevinokeh@gmail.com>
2018-11-19 22:42:10 +0100

    'fix merge errors'

--- server/src/db/migrations/j-create-reply.js
+++ server/src/db/migrations/j-create-reply.js
@@ -1,52 +1,4 @@
 export default {
-<<<<<<< HEAD
-  up: (queryInterface, Sequelize) => {
-    return queryInterface.createTable('Replies', {
-      id: {
-        allowNull: false,
-        primaryKey: true,
-        type: Sequelize.UUID,
-        defaultValue: Sequelize.UUIDV4
-      },
-      userId: {
-        required: true,
-        type: Sequelize.UUID,
-        onDelete: 'CASCADE',
-        references: {
-          model: 'Users',
-          key: 'id'
-        }
-      },
-      commentId: {
-        required: true,
-        type: Sequelize.UUID,
-        onDelete: 'CASCADE',
-        references: {
-          model: 'Comments',
-          key: 'id'
-        }
-      },
-      reply: {
-        required: true,
-        allowNull: false,
-        unique: false,
-        type: Sequelize.TEXT
-      },
-      createdAt: {
-        allowNull: false,
-        type: Sequelize.DATE
-      },
-      updatedAt: {
-        allowNull: false,
-        type: Sequelize.DATE
-      }
-    });
-  },
-  down: (queryInterface) => {
-    return queryInterface.dropTable('Replies');
-  }
-};
-=======
   up: (queryInterface, Sequelize) => queryInterface.createTable('Replies', {
     id: {
       allowNull: false,
@@ -89,4 +41,3 @@ export default {
   }),
   down: queryInterface => queryInterface.dropTable('Replies')
 };
->>>>>>> develop
--- server/src/db/models/comments.js
+++ server/src/db/models/comments.js
@@ -27,10 +27,7 @@ export default (sequelize, DataTypes) => {
     Comment.belongsTo(models.User, { foreignKey: 'userId' });
     Comment.belongsTo(models.Article, { foreignKey: 'articleId' });
     Comment.hasMany(models.Reply, { foreignKey: 'commentId' });
-<<<<<<< HEAD
     Comment.hasOne(models.Highlights, { foreignKey: 'commentId' });
-=======
->>>>>>> develop
   };
   return Comment;
 };
