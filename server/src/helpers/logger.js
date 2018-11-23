c985dce3dedce3fddd2e589a8500960f3cf8d716
Chukwuemeka Ajima <chukwuemekaajima@Chukwuemekas-MacBook-Pro.local>
2018-11-23 12:18:00 +0100

    BugFix(verification) Fix bug that prevent users from being verified:
    - Add a route to verify a user
    - Validate functionality in postman
    
    [Delivers #162167188]

--- server/src/helpers/logger.js
+++ server/src/helpers/logger.js
@@ -1,9 +1,21 @@
 import fileSystem from 'fs';
 import root from 'app-root-path';
 import util from 'util';
+import path from 'path';
 
 
 const logger = {};
+
+const isDir = (filePath) => {
+  const dirname = path.dirname(filePath);
+  if (fileSystem.existsSync(dirname)) return true;
+  isDir(dirname);
+  fileSystem.mkdirSync(dirname);
+};
+
+isDir(`${root}/logs/info.log`);
+isDir(`${root}/logs/error.log`);
+
 
 const infoStream = fileSystem.createWriteStream(`${root}/logs/info.log`, { flags: 'a' });
 const errorStream = fileSystem.createWriteStream(`${root}/logs/error.log`, { flags: 'a' });
